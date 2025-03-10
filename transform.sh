0ad22a19957e5a66bdb50ff36f331464c7c79f51
Zeeshan Ahmad <ziishaned@gmail.com>
2025-03-10 10:37:16 -0400

    initial commit

--- transform.sh
+++ transform.sh
@@ -8,12 +8,12 @@ cd JetBrains-Mono
 
 # Non-italic versions
 for f in $(fd -HI JetBrainsMono -e otf -E '*Italic.otf'); do
-  pyftfeatfreeze -f "ss02,cv03,cv04,cv15,cv16,cv18,cv19,cv20" -R "Mono/Mono $fontSuffix $fontVersion" -v $f "${f%.otf}.z.otf"
+  pyftfeatfreeze -f "ss02,cv03,cv04,cv15,cv16,cv18,cv19,cv20" -R "Mono/Mono $fontSuffix" -v $f "${f%.otf}.z.otf"
 done
 
 # Italicized versions
 for f in $(fd -HI Italic.otf -e otf); do
-  pyftfeatfreeze -f "ss02,cv03,cv04,cv15,cv16,cv18,cv19,cv20" -R "Mono/Mono $fontSuffix $fontVersion" -v $f "${f%.otf}.z.otf"
+  pyftfeatfreeze -f "ss02,cv03,cv04,cv15,cv16,cv18,cv19,cv20" -R "Mono/Mono $fontSuffix" -v $f "${f%.otf}.z.otf"
 done
 
 mv *.z.otf ../"JetBrains-Mono-$fontSuffix"/
