3c64e89a94e3006a55de504e8120feb0da0fa5da
Zeeshan Ahmad <ziishaned@gmail.com>
2025-03-10 17:08:45 -0400

    chore: minor changes

--- transform.sh
+++ transform.sh
@@ -1,35 +1,37 @@
 #!/bin/zsh
 
-rm -rf FontPatcher
-rm -rf FontPatcher.zip
-
-curl -LO https://github.com/ryanoasis/nerd-fonts/releases/download/v3.3.0/FontPatcher.zip
-unzip -d FontPatcher FontPatcher.zip
-
+# Variables
 fontSuffix="Z"
 fontVersion="2.304"
+outDir="JetBrains-Mono-$fontSuffix"
+patcherRepo="https://github.com/ryanoasis/nerd-fonts/releases/download/v3.3.0/FontPatcher.zip"
 
-mkdir -p "JetBrains-Mono-$fontSuffix"
+# Clean up and setup
+rm -rf FontPatcher FontPatcher.zip
+mkdir -p "$outDir"
+
+# Download and extract patcher
+curl -LO "$patcherRepo"
+unzip -d FontPatcher FontPatcher.zip
+
+# Process fonts
 cd JetBrains-Mono
 
-# Non-italic versions
-for f in $(fd -HI JetBrainsMono -e otf -E '*Italic.otf'); do
-  pyftfeatfreeze -f "ss02,cv03,cv04,cv15,cv16,cv18,cv19,cv20" -R "Mono/Mono $fontSuffix" -v $f "${f%.otf}.z.otf"
-done
+# Process all fonts with a single pattern (handles both italic and non-italic)
+fd -HI '\.otf$' -x pyftfeatfreeze -f "ss02,cv03,cv04,cv15,cv16,cv18,cv19,cv20" -R "Mono/Mono $fontSuffix" -v {} "{.}.z.otf" \;
 
-# Italicized versions
-for f in $(fd -HI Italic.otf -e otf); do
-  pyftfeatfreeze -f "ss02,cv03,cv04,cv15,cv16,cv18,cv19,cv20" -R "Mono/Mono $fontSuffix" -v $f "${f%.otf}.z.otf"
-done
+# Move processed fonts to output directory
+mv *.z.otf ../"$outDir"/
 
-mv *.z.otf ../"JetBrains-Mono-$fontSuffix"/
-cd ../"JetBrains-Mono-$fontSuffix"/
+# Rename files properly
+cd ../"$outDir"/
 for f in *.z.otf; do
-  mv $f "$(echo "$f" | sed s/"\.z"/"-$fontSuffix"/)"
+  mv "$f" "$(echo "$f" | sed 's/\.z/-'"$fontSuffix"'/')"
 done
 
 for f in *.otf; do
-    fontforge -script ../FontPatcher/font-patcher $f --out ../JetBrains-Mono-$fontSuffix --name "${f%.otf}" --octicons
+  fontforge -script ../FontPatcher/font-patcher "$f" --out "../$outDir" --name "${f%.otf}" --octicons
 done
 
+# Open the folder
 open .
